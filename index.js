847da6b77c97911bf74fe3766f40009e29f70853
Luca <gabadadzeluca@gmail.com>
2022-11-08 00:49:19 +0400

    add js logic

--- index.js
+++ index.js
@@ -1,35 +1,74 @@
-const billInput = document.getElementById('bill');
-const peopleInput = document.getElementById('people-count');
-const costumInput = document.getElementById('costum');
-const resetBtn = document.getElementById('reset-btn');
+// selecting tip %
 const tips = document.querySelectorAll('.tip');
+tips.forEach(tip =>{
+    tip.addEventListener('click', function(event){
+        calculateTip();
+        tips.forEach(tip => {
+            tip.classList.remove('active-tip');
+            if(event.target.classList == tip.classList){
+                // clear out costum tip field
+                costumTip.value = '';
+                event.target.classList.add('active-tip');
+            }
+        });
+    })
+})
 
-// billInput.addEventListener('input', billFunction);
-// peopleInput.addEventListener('input', peopleFunction);
-costumInput.addEventListener('input', costumFunction);
-// resetBtn.addEventListener('click', resetAll);
-tips.forEach(tip => tip.addEventListener('click', selectTip));
+// Bill input
+const billInput = document.getElementById('bill');
+billInput.addEventListener('input', calculateTip);
+
+// Number of people
+const numPeople = document.getElementById('people-count');
+numPeople.addEventListener('input', calculateTip);
 
 
-// select tip by click
-function selectTip(event){
+// Costum tip field
+const costumTip = document.getElementById('costum');
+costumTip.addEventListener('input', ()=>{
     tips.forEach(tip => {
         tip.classList.remove('active-tip');
-        if(event.target.classList == tip.classList){
-            event.target.classList.add('active-tip');
-        }
     });
+    calculateTip();
+});
+
+// Reset btn
+const resetBtn = document.getElementById('reset-btn');
+resetBtn.addEventListener('click', ()=>{
+    //clear out fields !
+    tips.forEach(tip =>{
+        tip.classList.remove('active-tip');
+    })
+})
+
+
+
+
+// Calculate output
+function calculateTip(){
+    let bill = parseFloat(billInput.value);
+    let nofPeople = parseFloat(numPeople.value);
+    let tip = 0;
+    if(document.querySelector('.active-tip')){
+        tip = parseFloat(document.querySelector('.active-tip').innerHTML);
+    }else{
+        tip = parseFloat(costumTip.value);
+    }
+
+    let total = bill + (bill*tip/100);
+    let totalPricePerson = ((total / nofPeople)).toFixed(2);
+    let totalTipPerson = (bill*tip/100)/nofPeople;
+    display(totalTipPerson, totalPricePerson);
 }
 
-
-function costumFunction(){
-    tips.forEach(tip => {
-            tip.classList.remove('active-tip');
-        })
-    //
-    //
-
+function display(x,y){
+    if(isNaN(x) || isNaN(y)){
+        console.log("NANNNNANN")
+        x = y = '';
+        
+    }
+    const totalTipPerson = document.getElementById('tip-total');
+    totalTipPerson.innerHTML = '$' + x;
+    const totalPricePerson = document.getElementById('total');
+    totalPricePerson.innerHTML = '$' +  y;
 }
-
-
-
