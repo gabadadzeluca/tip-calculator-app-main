493013c26d8d548de1354d6457124a0e584ad189
Luca <gabadadzeluca@gmail.com>
2022-11-09 00:19:06 +0400

    finish js

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ billInput.addEventListener('input', calculateTip);
 // Number of people
 const numPeople = document.getElementById('people-count');
 numPeople.addEventListener('input', calculateTip);
+// error message
+const errorMsg = document.querySelector('.error-msg-people');
+
+//number inputs
+const inputs = document.querySelectorAll('input[type=number]');
 
 
 // Costum tip field
@@ -32,6 +37,7 @@ costumTip.addEventListener('input', ()=>{
     calculateTip();
 });
 
+
 // Reset btn
 const resetBtn = document.getElementById('reset-btn');
 resetBtn.addEventListener('click', ()=>{
@@ -39,13 +45,17 @@ resetBtn.addEventListener('click', ()=>{
     tips.forEach(tip =>{
         tip.classList.remove('active-tip');
     })
+    inputs.forEach(input =>{
+        input.value = '';
+    })
+    resetBtn.style.backgroundColor = '#0D686D';
 })
-
 
 
 
 // Calculate output
 function calculateTip(){
+    resetBtn.style.backgroundColor = '#26C2AE';
     let bill = parseFloat(billInput.value);
     let nofPeople = parseFloat(numPeople.value);
     let tip = 0;
@@ -54,10 +64,28 @@ function calculateTip(){
     }else{
         tip = parseFloat(costumTip.value);
     }
+    //check number of people
+    if(numPeople.value.length > 0 && numPeople.value == 0){
+        errorMsg.style.display = 'block';
+    }else{
+        errorMsg.style.display = 'none';
+    }
+
+    // Only acceptable input form
+    const pattern = '^[0-9]*$';
+    // const inputs = document.querySelectorAll('input[type=number]');
+    // console.log(inputs);
+    inputs.forEach(input => {
+        if(! input.value.match(pattern)){
+        input.value = '';
+        console.log('yep')
+        }
+    })
+
 
     let total = bill + (bill*tip/100);
     let totalPricePerson = ((total / nofPeople)).toFixed(2);
-    let totalTipPerson = (bill*tip/100)/nofPeople;
+    let totalTipPerson = ((bill*tip/100)/nofPeople).toFixed(2);
     display(totalTipPerson, totalPricePerson);
 }
 
